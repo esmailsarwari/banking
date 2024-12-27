f9123700ec4d43303fd29b129ee1aee560885609
esmail sarwari <esmail.sarwari10@gmail.com>
2024-12-26 19:27:39 -0800

    bugFixed | request loan functionality uptodate

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const account1 = {
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
+    type: 'premium',
 };
 
 const account2 = {
@@ -13,6 +14,7 @@ const account2 = {
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
+    type: 'standard',
 };
 
 const account3 = {
@@ -20,6 +22,7 @@ const account3 = {
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
+    type: 'premium',
 };
 
 const account4 = {
@@ -27,6 +30,7 @@ const account4 = {
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 4444,
+    type: 'basic',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -191,7 +195,7 @@ const moneyTransferFunctionality = (e) => {
 const requestLoanFunctionality = (e) => {
     e.preventDefault();
 
-    const loanAmount = Number(inputLoanAmount.value.trim());
+    const loanAmount = Math.round(Number(inputLoanAmount.value.trim()));
 
     if (
         loanAmount > 0 &&
